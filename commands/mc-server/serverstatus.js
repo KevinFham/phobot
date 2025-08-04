7189b8dbe7fe932529ef0a2018d19c08ee2797ac
KevinFham <kevin.pha01@gmail.com>
2025-08-04 17:07:38 -0500

    edit loop for server status

--- commands/mc-server/serverstatus.js
+++ commands/mc-server/serverstatus.js
@@ -16,14 +16,18 @@ const StatusDiscordEmoji = {
     "UNKNOWN": ":question:"
 }
 
+const SERVER_STATUS_REFRESH_MS = 10 * 1000;
+const STATUS_REFRESH_DURATION_MS = 5 * 60 * 1000;
+
 function printServerStatusResponse(statusObj){
     let responseStr = "";
-    console.log(statusObj)
     responseStr += "`Machine Status`: " + statusObj.machineStatus + " " + StatusDiscordEmoji[statusObj.machineStatus];
     responseStr += "\n`Minecraft Server Status`: " + statusObj.mcServerStatus + " " + StatusDiscordEmoji[statusObj.mcServerStatus];
     if (statusObj.mcServerStatus == ServerStatus.ACTIVE) {
         responseStr += "\n`Currently Online:`";
-        statusObj.mcServerPlayers.forEach(playerUname => responseStr += `\n\t- ${playerUname}`);
+        if (statusObj.mcServerPlayers.length > 0) {
+            statusObj.mcServerPlayers.forEach(playerUname => responseStr += `\n> ${playerUname}`);
+        }
     }
     return responseStr;
 }
@@ -43,27 +47,33 @@ async function execute (interaction) {
 
     interaction.deferReply();
 
-    var { stdout, stderr } = await exec_p(`fping -c1 -t600 ${process.env.SERVER_IP_ADDR}`);
-    if (!stdout.includes("timed out")) {                // Machine is Up
-        serverStatusObj.machineStatus = ServerStatus.ACTIVE;
+    var refreshIntervalID = setInterval(async () => {
 
-        var { stdout, stderr } = await exec_p(`ssh -t root@${process.env.SERVER_IP_ADDR} "docker container inspect -f '{{.State.Status}}, {{.State.Health}}' ${process.env.GOOPCRAFT_CONTAINER_NAME}"`);
-        if (stdout.includes("running") && stdout.includes("healthy")) {     // MC Server is up and running
-            serverStatusObj.mcServerStatus = ServerStatus.ACTIVE;
-            var { stdout, stderr } = await exec_p(`ssh -t root@${process.env.SERVER_IP_ADDR} "docker exec ${process.env.GOOPCRAFT_CONTAINER_NAME} rcon-cli \"list\" | sed -e 's/\x1b\[[0-9;]*m//g' -e 's/^[0-9a-zA-Z ]*: '//g -e 's/ //g'"`);
-            stdout.trim().split(",").forEach(player => serverStatusObj.mcServerPlayers.push(player));
-        } else if (stdout.includes("starting")) {                           // MC Server is booting up
-            serverStatusObj.mcServerStatus = ServerStatus.STARTING;
-        } else if (stdout.includes("exited")) {                             // MC Server is shut down
+        var { stdout, stderr } = await exec_p(`fping -c1 -t600 ${process.env.SERVER_IP_ADDR}`);
+        if (!stdout.includes("timed out")) {                // Machine is Up
+            serverStatusObj.machineStatus = ServerStatus.ACTIVE;
+
+            var { stdout, stderr } = await exec_p(`ssh -t root@${process.env.SERVER_IP_ADDR} "docker container inspect -f '{{.State.Status}}, {{.State.Health}}' ${process.env.GOOPCRAFT_CONTAINER_NAME}"`);
+            if (stdout.includes("running") && stdout.includes("healthy")) {     // MC Server is up and running
+                serverStatusObj.mcServerStatus = ServerStatus.ACTIVE;
+                var { stdout, stderr } = await exec_p(`ssh -t root@${process.env.SERVER_IP_ADDR} "docker exec ${process.env.GOOPCRAFT_CONTAINER_NAME} rcon-cli \"list\" | sed -e 's/\x1b\[[0-9;]*m//g' -e 's/^[0-9a-zA-Z ]*: '//g -e 's/ //g'"`);
+                serverStatusObj.mcServerPlayers = stdout.trim().split(",").filter(x => x);
+            } else if (stdout.includes("starting")) {                           // MC Server is booting up
+                serverStatusObj.mcServerStatus = ServerStatus.STARTING;
+            } else if (stdout.includes("exited")) {                             // MC Server is shut down
+                serverStatusObj.mcServerStatus = ServerStatus.STOPPED;
+            } //else, MC Server status unknown
+
+        } else if (stdout.includes("timed out")) {          // Machine is Down
+            serverStatusObj.machineStatus = ServerStatus.STOPPED;
             serverStatusObj.mcServerStatus = ServerStatus.STOPPED;
-        } //else, MC Server status unknown
+        }
 
-    } else if (stdout.includes("timed out")) {          // Machine is Down
-        serverStatusObj.machineStatus = ServerStatus.STOPPED;
-        serverStatusObj.mcServerStatus = ServerStatus.STOPPED;
-    }
+        await interaction.editReply(printServerStatusResponse(serverStatusObj));
 
-    await interaction.editReply(printServerStatusResponse(serverStatusObj));
+    }, SERVER_STATUS_REFRESH_MS);
+
+    setTimeout(() => { clearInterval(refreshIntervalID); }, STATUS_REFRESH_DURATION_MS);
 }
 
 export { data, execute };
